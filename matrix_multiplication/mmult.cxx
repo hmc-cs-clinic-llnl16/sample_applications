35ddb712ab51863c12be81183ec8ee58876c8935
Dan Obermiller <dobermiller16@cmc.edu>
2016-09-11 21:18:36 -0700

    Add OpenMP policy and timings

--- matrix_multiplication/mmult.cxx
+++ matrix_multiplication/mmult.cxx
@@ -7,6 +7,9 @@
 #include <vector>
 #include <type_traits>
 #include <random>
+#include <string>
+#include <sstream>
+#include <stdexcept>
 
 #include "RAJA/RAJA.hxx"
 
@@ -16,8 +19,9 @@ class Matrix {
   RAJA::Index_type cols_;
   std::vector<T> data_;
 public:
+  static constexpr char* name = Policy::name;
   Matrix() { }
-
+  
   Matrix(const RAJA::Index_type rows, const RAJA::Index_type cols, const T& defaultValue = T())
       : rows_(rows), cols_(cols), data_(rows_ * cols_, defaultValue) { }
 
@@ -34,19 +38,19 @@ public:
       RAJA::RangeSegment(0, rows_),
       RAJA::RangeSegment(0, cols_),
       [=](RAJA::Index_type row, RAJA::Index_type col) {
-//        std::cout << row << ":" << col;
         double result = 0;
         for (RAJA::Index_type k = 0; k < rows_; ++k) {
-//          std::cout << "\nRhs: " << rhsView(row, k) << "\nLhs: " << lhsView(k, col);
           result += rhsView(row, k) * lhsView(k, col);
         }
-  //      std::cout << "\nResult: " << result;
         resultView(row, col) = result;
       }
     );
 
     return result;
   }
+
+  RAJA::Index_type getRows() const { return rows_; }
+  RAJA::Index_type getCols() const { return cols_; }
 
   T& operator()(const RAJA::Index_type row, const RAJA::Index_type col) {
     return data_[row * cols_ + col];
@@ -63,6 +67,16 @@ struct SerialPolicy {
     RAJA::Permute<RAJA::PERM_IJ>
   >;
   using VIEW = RAJA::View<double, RAJA::Layout<int, RAJA::PERM_IJ, int, int>>;
+  static constexpr char* name = "Serial";
+};
+
+struct OmpPolicy {
+  using EXEC = RAJA::NestedPolicy<
+    RAJA::ExecList<RAJA::omp_parallel_for_exec, RAJA::seq_exec>,
+    RAJA::Permute<RAJA::PERM_IJ>
+  >;
+  using VIEW = RAJA::View<double, RAJA::Layout<int, RAJA::PERM_IJ, int, int>>;
+  static constexpr char* name = "OMP";
 };
 
 template <typename T>
@@ -82,16 +96,46 @@ std::vector<T> mult(const std::vector<T>& lhs, const std::vector<T>& rhs, const 
   return resultV;
 }
 
+template <typename MATRIX>
+void runTimingText(MATRIX& left, MATRIX& right, const std::vector<double>& result, const unsigned numTrials, double* timeElapsed) {
+    *timeElapsed = std::numeric_limits<double>::max();
+    for (std::size_t i = 0; i < numTrials; ++i) {
+      auto tic = std::chrono::high_resolution_clock::now();
+      auto actualResult = left * right;
+      auto toc = std::chrono::high_resolution_clock::now();
+      auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(toc - tic).count();
+      std::cout << "Completed iteration " << i + 1 << " of " << numTrials << " for variant " << MATRIX::name << " in " << elapsed << " seconds\n";
+      checkResult(actualResult, result);
+      *timeElapsed = std::min(elapsed, *timeElapsed);
+    }
+}
+
+template <typename MATRIX>
+void checkResult(const MATRIX& actual, const std::vector<double>& expected) {
+    for (std::size_t i = 0; i < actual.getRows(); ++i) {
+      for (std::size_t j = 0; j < actual.getCols(); ++j) {
+        if (actual(i, j) != expected[i * actual.getCols() + j]) {
+          std::ostringstream os;
+          os << "Invalid value at: (" << i << ", " << j << "). Was " << actual(i, j) << " but expected " << expected[i * actual.getCols()  + j] << "\n";
+          throw std::runtime_error(os.str());
+        }
+      }
+    }
+}
+
 int main(int argc, char** argv) {
-  constexpr static RAJA::Index_type NUM_ROWS = 10;
-  constexpr static RAJA::Index_type NUM_COLS = 10;
-  
+  constexpr static RAJA::Index_type NUM_ROWS = 1000;
+  constexpr static RAJA::Index_type NUM_COLS = 1000;
+  constexpr static std::size_t NUM_TRIALS = 10;
+
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<double> dist(0, 5);
 
   auto matrix1 = Matrix<double, SerialPolicy>(NUM_ROWS, NUM_COLS);
   auto matrix2 = Matrix<double, SerialPolicy>(NUM_ROWS, NUM_COLS);
+  auto matrix3 = Matrix<double, OmpPolicy>(NUM_ROWS, NUM_COLS);
+  auto matrix4 = Matrix<double, OmpPolicy>(NUM_ROWS, NUM_COLS);
 
   auto control1 = std::vector<double>(NUM_ROWS * NUM_COLS);
   auto control2 = std::vector<double>(NUM_ROWS * NUM_COLS);
@@ -100,22 +144,37 @@ int main(int argc, char** argv) {
     for (std::size_t j = 0; j < NUM_COLS; ++j) {
       double first = dist(gen), second = dist(gen);
       matrix1(i, j) = first;
+      matrix3(i, j) = first;
       control1[i * NUM_COLS + j] = first;
       matrix2(i, j) = second;
+      matrix4(i, j) = second;
       control2[i * NUM_COLS + j] = second;
     }
   }
 
-  auto result = matrix1 * matrix2;
+  double bestTime = 0;
   auto resultV = mult(control1, control2, NUM_ROWS, NUM_COLS);
-
-  for (std::size_t i = 0; i < NUM_ROWS; ++i) {
-    for (std::size_t j = 0; j < NUM_COLS; ++j) {
-      if (result(i, j) != resultV[i * NUM_COLS + j]) {
-        std::cout << "Invalid value at: (" << i << ", " << j << "). Was " << result(i, j) << " but expected " << resultV[i * NUM_COLS + j] << std::endl;
-        return 1;
-      }
-    }
+  try {
+    runTimingText(matrix1, matrix2, resultV, NUM_TRIALS, &bestTime);
+  } catch (std::runtime_error e) {
+    std::cout << e.what() << std::endl;
+    return 1;
   }
+
+  std::cout << "Completed matrix multiplication serial style without error.\n"  
+            << NUM_ROWS << " rows and " << NUM_COLS << " columns with a best time of " 
+            << bestTime << " seconds." << std::endl;
+
+  bestTime = 0;
+  try {
+    runTimingText(matrix3, matrix4, resultV, NUM_TRIALS, &bestTime);
+  } catch (std::runtime_error e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+
+  std::cout << "Completed matrix multiplication omp style without error.\n"  
+            << NUM_ROWS << " rows and " << NUM_COLS << " columns with a best time of " 
+            << bestTime << " seconds." << std::endl;
 }
 
