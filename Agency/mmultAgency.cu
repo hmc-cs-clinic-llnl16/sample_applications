c1df9d194eaa7104c5d845bd300c01a8f6d50d72
joanthanFinnell <jonathan.finnell@gmail.com>
2016-09-17 11:52:32 -0700

    Parallel execution added

--- Agency/mmultAgency.cu
+++ Agency/mmultAgency.cu
@@ -3,21 +3,28 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
-#include <algorithm>
+
+using namespace agency;
+// allocate data in GPU memory
+using vector = std::vector<size_t, cuda::managed_allocator<size_t>>;
+using matrix = std::vector<vector, cuda::managed_allocator<vector>>;
+
+void verifyAndCleanMatrix(size_t n, matrix* c_ptr) {
+    for (auto i = c_ptr->begin(); i < c_ptr->end(); ++i) {
+        for (auto j = i -> begin(); j < i -> end(); ++j) {
+            assert(*j == n);
+            *j = 0;
+        };
+    }
+}
 
 int main()
 {
-    using namespace agency;
-    // allocate data in GPU memory
-    using vector = std::vector<size_t, cuda::managed_allocator<size_t>>;
-    using matrix = std::vector<vector, cuda::managed_allocator<vector>>;
     size_t n = 1 << 8;
     matrix a(n, vector(n, 1));
     matrix b(n, vector(n, 1));
     matrix c(n, vector(n, 0));
-    vector* a_ptr = a.data();
-    vector* b_ptr = b.data();
-    vector* c_ptr = c.data();
+
     // execute sequentially in the current thread
     bulk_invoke(seq(n*n), [=](sequenced_agent& self)
     {
@@ -25,24 +32,24 @@ int main()
         int col = self.index() % n;
 
         for (int k = 0; k < n; ++k) {
-            c_ptr[row].data()[col] += a_ptr[row].data()[k] * b_ptr[k].data()[col];
+            c.data()[row].data()[col] += a.data()[row].data()[k] * b.data()[k].data()[col];
         }
     });
-    for (auto i = c.begin(); i < c.end(); ++i) {
-        for (auto j = i -> begin(); j < i -> end(); ++j) {
-            assert(*j == n);
-            *j = 0;
-        };
-    }
-    /*// execute in parallel on the CPU
-    bulk_invoke(par(n), [=](parallel_agent& self)
+    verifyAndCleanMatrix(n, &c);
+
+    // execute in parallel on the CPU
+    bulk_invoke(par(n*n), [=](parallel_agent& self)
     {
-        int i = self.index();
-        z_ptr[i] = a * x_ptr[i] + y_ptr[i];
+        int row = self.index() / n;
+        int col = self.index() % n;
+
+        for (int k = 0; k < n; ++k) {
+            c.data()[row].data()[col] += a.data()[row].data()[k] * b.data()[k].data()[col];
+        }
     });
-    assert(z == reference);
-    std::fill(z.begin(), z.end(), 0);
-    // execute in parallel on a GPU
+    verifyAndCleanMatrix(n, &c);
+
+    /*// execute in parallel on a GPU
     cuda::grid_executor gpu;
     bulk_invoke(par(n).on(gpu), [=] __device__ (parallel_agent& self)
     {
